efb74f3a92bcc0d0eec081be3c820ba31f1f28ec
wuchao <wuchao076@pingan.com.cn>
2016-07-17 23:08:34 +0800

    wuuchao

---
+++ app/src/main/java/wuuchao/github/com/libarary/BannerRecycler.java
@@ -0,0 +1,35 @@
+package wuuchao.github.com.libarary;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * 描述:
+ * 作者: wuuchao
+ * 时间: 2016/07/17 11:21
+ */
+public class BannerRecycler extends ViewGroup {
+    public BannerRecycler(Context context) {
+        super(context);
+    }
+
+    public BannerRecycler(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BannerRecycler(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    public BannerRecycler(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
