ac89be563d8473d2fe46a9c7ea9ef8d413e2b3ef
chao wu <chao wu>
2016-08-18 19:39:30 +0800

    0.5

--- app/src/main/java/wuuchao/github/com/libarary/BannerRecycler.java
+++ app/src/main/java/wuuchao/github/com/libarary/BannerRecycler.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Environment;
@@ -16,6 +18,9 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import android.widget.ImageView;
 import android.widget.Scroller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import wuuchao.github.com.bannerrecycler.R;
 import wuuchao.github.com.libarary.utils.DeviceUtil;
@@ -29,19 +34,28 @@ public class BannerRecycler extends View {
     private static final int TOUCH_STATE_SCROLLING = 1;
     private static final int TOUCH_STATE_SCROLLED = 2;
     private static final int SNAP_VELOCITY = 1000;
+    private static final int SCROLL_TO_LEFT = 101;
+    private static final int SCROLL_TO_RIGHT = 102;
+    private static final int SCROLL_TO_NONE = 103;
+    public static final int MAX_DRAW_BITMAPS = 3; //在屏幕上绘制的图片数量
     private Context mContext;
     private Scroller mScroller;
-    private int[] drawBitmaps = new int[2];
+    private int[] drawBitmaps = new int[3];
     private int mCurX;
     private boolean startScroll;
     private int mTouchSlop;
     private int mMaximumVelocity;
     private VelocityTracker mVelocityTracker;
-    private int mTouchState;
-    private int mPreIndex, mIndex;
+    private int mTouchState; //滚动的状态
+    private int mScrollDirect; //滚动的方向
+    private int mIndex;
+    private int mRealIndex;//根据mIndex计算的当前页码
     private float mLastMotionX;
     private BitmapDrawable[] d = new BitmapDrawable[3];
-    private Bitmap[] b = new Bitmap[3];
+    private Bitmap[] b = new Bitmap[MAX_DRAW_BITMAPS];
+    private RectF[] mWidgetRect = new RectF[MAX_DRAW_BITMAPS];
+    private Paint mPaint;
+    private List<Bitmap> mData = new ArrayList<>();
 
     public BannerRecycler(Context context) {
         super(context);
@@ -66,19 +80,48 @@ public class BannerRecycler extends View {
 
     private void init(Context context) {
         mContext = context;
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         drawBitmaps[0] = R.mipmap.splash1;
         drawBitmaps[1] = R.mipmap.ic_launcher;
+        drawBitmaps[2] = R.mipmap.psu;
         ViewConfiguration configuration = ViewConfiguration
                 .get(getContext());
         mTouchSlop = configuration.getScaledTouchSlop();
         mMaximumVelocity = configuration.getScaledMaximumFlingVelocity();
         mScroller = new Scroller(context);
-        b[0] = BitmapFactory.decodeResource(mContext.getResources(), drawBitmaps[drawBitmaps.length - 1]);
-        b[1] = BitmapFactory.decodeResource(mContext.getResources(), drawBitmaps[0]);
-        b[2] = BitmapFactory.decodeResource(mContext.getResources(), drawBitmaps[1]);
+        b[0] = BitmapFactory.decodeResource(mContext.getResources(), drawBitmaps[0]);
+        b[1] = BitmapFactory.decodeResource(mContext.getResources(), drawBitmaps[1]);
+        b[2] = BitmapFactory.decodeResource(mContext.getResources(), drawBitmaps[2]);
+        mData.add(b[0]);
+        mData.add(b[1]);
+        mData.add(b[2]);
         d[0] = new BitmapDrawable(mContext.getResources(), b[drawBitmaps.length - 1]);
         for (int index = 1; index < d.length; index++) {
             d[index] = new BitmapDrawable(mContext.getResources(), b[index - 1]);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidgetRect[0] = new RectF(-getMeasuredWidth(), 0, 0, getMeasuredHeight());
+        mWidgetRect[1] = new RectF(0, 0, getMeasuredWidth(), getMeasuredHeight());
+        mWidgetRect[2] = new RectF(getMeasuredWidth(), 0, getMeasuredWidth() * 2, getMeasuredHeight());
+    }
+
+    /**
+     * 计算实际的页码
+     */
+    private void resetPageIndex() {
+        if (mScrollDirect == SCROLL_TO_LEFT) {
+            mRealIndex--;
+        } else if (mScrollDirect == SCROLL_TO_RIGHT) {
+            mRealIndex++;
+        }
+        if (mRealIndex < 0) {
+            mRealIndex = mData.size() - 1;
+        } else if (mRealIndex > mData.size() - 1) {
+            mRealIndex = 0;
         }
     }
 
@@ -117,12 +160,14 @@ public class BannerRecycler extends View {
                 if (mTouchState == TOUCH_STATE_SCROLLING) {
                     mVelocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
                     int velocityX = (int) mVelocityTracker.getXVelocity();
-                    mPreIndex = mIndex;
                     if (velocityX > SNAP_VELOCITY) {
+                        mScrollDirect = SCROLL_TO_LEFT;
                         snapToScreen(--mIndex);
                     } else if (velocityX < -SNAP_VELOCITY) {
+                        mScrollDirect = SCROLL_TO_RIGHT;
                         snapToScreen(++mIndex);
                     } else {
+                        mScrollDirect = SCROLL_TO_NONE;
                         snapToScreen(mIndex);
                     }
                     if (null != mVelocityTracker) {
@@ -143,13 +188,13 @@ public class BannerRecycler extends View {
     }
 
     private void resetView() {
-        mCurX = mScroller.getCurrX();
-        int curIndex = Math.abs(mIndex % drawBitmaps.length);
-        d[0] = new BitmapDrawable(mContext.getResources(), b[curIndex - 1 < 0 ? b.length - 1 : curIndex - 1]);
-        for (int index = 1; index < d.length; index++) {
-            d[1] = new BitmapDrawable(mContext.getResources(), b[curIndex + index - 1]);
+        if (mScrollDirect != SCROLL_TO_NONE) {
+            mWidgetRect[0] = new RectF(getMeasuredWidth() * (mIndex - 1), 0, getMeasuredWidth() * mIndex, getMeasuredHeight());
+            mWidgetRect[1] = new RectF(getMeasuredWidth() * mIndex, 0, getMeasuredWidth() * (mIndex + 1), getMeasuredHeight());
+            mWidgetRect[2] = new RectF(getMeasuredWidth() * (mIndex + 1), 0, getMeasuredWidth() * (mIndex + 2), getMeasuredHeight());
+            resetPageIndex();
+            invalidate();
         }
-        invalidate();
     }
 
     @Override
@@ -168,9 +213,15 @@ public class BannerRecycler extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for (int index = 0; index < d.length; index++) {
-            d[index].setBounds(mCurX - (-1 * (index - 1) * getWidth()), 0, mCurX + index * getWidth(), getHeight());
-            d[index].draw(canvas);
+        for (int index = 0; index < MAX_DRAW_BITMAPS; index++) {
+            int drawPosition = mRealIndex + index - 1;
+            if (drawPosition < 0) {
+                drawPosition = mData.size() - 1;
+            } else if (drawPosition > mData.size() - 1) {
+                drawPosition = 0;
+            }
+            Log.e("onDraw", "drawPosition=" + drawPosition + "index=" + index + "mData.get(drawPosition)=" + mData.get(drawPosition).toString());
+            canvas.drawBitmap(mData.get(drawPosition), null, mWidgetRect[index], mPaint);
         }
     }
 }
